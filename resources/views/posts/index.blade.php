0dbc1b1d350fc3251f65535ebb39a477b0d0e77d
oguzhanarslan <oguzhan28@hotmail.nl>
2017-11-15 12:00:39 +0100

    delete

--- resources/views/posts/index.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layout')
-
-@section('content')
-<p>ddfsdfsdgsfd</p>
-@endsection('content')
--- resources/views/tasks/show.blade.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>about</title>
-</head>
-<body>
-                <h1>{{ $task->body }}</h1>
-</body>
-</html>
