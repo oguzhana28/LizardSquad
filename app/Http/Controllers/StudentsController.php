d0ee829ee39d1c901ee60e9fdf05778d1bfa458f
Lars Luijendijk <larsluijendijk@gmail.com>
2017-12-05 10:34:17 +0100

    Fixed Student Create (No image yet)
    Fixed Student Create (No image yet)

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -5,11 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
-use App\Http\Requests;
+//use App\Http\Requests;
 use App\Student;
 
 class StudentsController extends Controller
 {
+
     public function index()
     {       
     	$students = Student::getStudents();    
@@ -21,19 +22,19 @@ class StudentsController extends Controller
         return view('createStudents');
     }
 
-    public function upload(){
+    public function upload(request $request){
 
 		if(Input::hasFile('file')){
 			$file = Input::file('file');
 			$result = Student::uploadfile($file);
 			$file->move('uploads', $result);
 		}
-		$firstname = $this->request->input('firstname');
-        $prefix = $this->request->input('prefix');	
-        $lastname = $this->request->input('lastname');	
-        $mobile = $this->request->input('mobile');	
-        $address = $this->request->input('address');	
-        $house_number = $this->request->input('house_number');	
+		$firstname = $request->input('firstname');
+        $prefix = $request->input('prefix');	
+        $lastname = $request->input('lastname');	
+        $mobile = $request->input('mobile');	
+        $address = $request->input('address');	
+        $house_number = $request->input('house_number');	
             
         $data = array('firstname'=>$firstname,'prefix'=>$prefix,'lastname'=>$lastname,'mobile'=>$mobile,'address'=>$address,'house_number'=>$house_number);
             
